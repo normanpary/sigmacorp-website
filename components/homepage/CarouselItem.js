a6503871af9d2e1f3de988a74f4d33956afacb24
wilfreastt <wilfreastt@gmail.com>
2021-12-29 11:32:16 -0400

    Loading _app.js

--- components/homepage/CarouselItem.js
+++ components/homepage/CarouselItem.js
@@ -1,4 +1,7 @@
 import useTranslation from 'next-translate/useTranslation'
+
+import Fade from 'react-reveal/Fade'
+
 export default function CarouselItem({ item }) {
   const { t } = useTranslation()
   return (
@@ -8,17 +11,19 @@ export default function CarouselItem({ item }) {
         src={item.image}
         alt="Carousel Item"
       />
-      <div className="absolute top-4 left-10 right-10 sm:p-6 md:top-8 md:left-16 lg:top-16">
-        <p className="text-pink-700 text-xl font-extrabold sm:text-4xl md:text-5xl ">
-          {item.title}
-        </p>
-        <p className="text-sm pb-2 sm:text-base md:text-xl">{item.sub_title}</p>
-        <input
-          className="bg-violeta text-white rounded-full sm:font-bold sm:py-1 px-4"
-          type="button"
-          value={t('home:conocelos')}
-        />
-      </div>
+      <Fade right cascade>
+        <div className="absolute top-4 left-10 right-10 sm:p-6 md:top-8 md:left-16 lg:top-16">
+          <p className="text-pink-700 text-xl font-extrabold sm:text-4xl md:text-5xl ">
+            {item.title}
+          </p>
+          <p className="text-sm pb-2 sm:text-base md:text-xl">{item.sub_title}</p>
+          <input
+            className="bg-violeta text-white rounded-full sm:font-bold sm:py-1 px-4"
+            type="button"
+            value={t('home:conocelos')}
+          />
+        </div>
+      </Fade>
     </div>
   )
 }
--- pages/index.js
+++ pages/index.js
@@ -10,6 +10,9 @@ import BlogCarousel from '@/components/homepage/BlogCarousel'
 import { URL_BLOG_POSTS } from '@/data/api/config'
 import axios from 'axios'
 import Carousel from '@/components/homepage/Carousel'
+import Fade from 'react-reveal/Fade'
+import Zoom from 'react-reveal/Zoom'
+import { useEffect, useState } from 'react'
 
 export default function HomePage({ posts, locale, availableLocales }) {
   const { t } = useTranslation()
@@ -22,56 +25,72 @@ export default function HomePage({ posts, locale, availableLocales }) {
         availableLocales={availableLocales}
       />
       <Carousel />
+
       <div className="p-4 flex justify-center">
-        <div className="grid grid-cols-2 gap-4 md:gap-3 sm:grid-cols-3 md:grid-cols-4 lg:flex lg:flex-row  lg:-mt-40">
-          {items_card.map((item, index) => (
-            <ItemCard item={item} key={index} index={index} />
-          ))}
-        </div>
+        <Fade right>
+          <div className="grid grid-cols-2 gap-4 md:gap-3 sm:grid-cols-3 md:grid-cols-4 lg:flex lg:flex-row  lg:-mt-40">
+            {items_card.map((item, index) => (
+              <ItemCard item={item} key={index} index={index} />
+            ))}
+          </div>
+        </Fade>
       </div>
+
       <div className=" space-y-6 md:space-y-0 p-8 grid grid-cols-1 md:grid-cols-2 md:gap-6 md:px-8 lg:px-28 bg-fondo_reverse bg-no-repeat bg-right-top">
-        <div>
-          <p className="font-extrabold text-gray-500 pb-4">{t('home:quienes_somos')}</p>
-          <p className="text-xl sm:text-2xl md:text-3xl text-pink-700 font-extrabold">
-            {t('home:laboratorio')}
-          </p>
-          <p className="text-xl sm:text-2xl md:text-3xl text-blue-800 font-extrabold pb-8">
-            {t('home:sigma')}
-          </p>
-          <p className="text-xs font-bold pb-4">{t('home:ofrecemos')}</p>
-          <p className="text-xs text-gray-600 pb-8">{t('home:proporcionamos')}</p>
-          <p className="text-xs underline font-semibold text-blue-800">{t('home:leer_mas')}</p>
-        </div>
+        <Fade bottom cascade>
+          <div>
+            <p className="font-extrabold text-gray-500 pb-4">{t('home:quienes_somos')}</p>
+            <p className="text-xl sm:text-2xl md:text-3xl text-pink-700 font-extrabold">
+              {t('home:laboratorio')}
+            </p>
+            <p className="text-xl sm:text-2xl md:text-3xl text-blue-800 font-extrabold pb-8">
+              {t('home:sigma')}
+            </p>
+            <p className="text-xs font-bold pb-4">{t('home:ofrecemos')}</p>
+            <p className="text-xs text-gray-600 pb-8">{t('home:proporcionamos')}</p>
+            <p className="text-xs underline font-semibold text-blue-800">{t('home:leer_mas')}</p>
+          </div>
+        </Fade>
         <div className="relative md:col-start-1 md:row-start-1">
-          <div className="hidden sm:flex justify-end w-full">
-            <img className="w-2/3" src="/static/images/home/children.png" />
-          </div>
-
-          <div className="flex w-full sm:w-60 sm:absolute sm:bottom-10 sm:left-10">
-            <ItemProposito />
-          </div>
+          <Zoom>
+            <div className="hidden sm:flex justify-end w-full">
+              <img className="w-2/3" src="/static/images/home/children.png" />
+            </div>
+          </Zoom>
+          <Fade left>
+            <div className="flex w-full sm:w-60 sm:absolute sm:bottom-10 sm:left-10">
+              <ItemProposito />
+            </div>
+          </Fade>
         </div>
       </div>
-      <div className="bg-gray-200 bg-fondo bg-no-repeat bg-left-top p-8 mt-8">
-        <p className="font-extrabold text-gray-500 text-center">{t('home:nuestro_blog')}</p>
 
-        <div className="flex justify-center mt-4">
-          <p className="text-xl sm:text-2xl md:text-3xl text-pink-700 font-extrabold">
-            {t('home:responsabilidad')}
-          </p>
-          <p className="text-xl sm:text-2xl md:text-3xl text-blue-800 font-extrabold pl-2">
-            {t('home:social')}
-          </p>
-        </div>
-        <input
-          className="bg-violeta text-white text-xs rounded-full py-1 px-4 mt-8 ml-8 mb-2"
-          type="button"
-          value={t('home:ver_blog')}
-        />
+      <div className="bg-gray-200 bg-fondo bg-no-repeat bg-left-top p-8 mt-8">
+        <Fade bottom>
+          <p className="font-extrabold text-gray-500 text-center">{t('home:nuestro_blog')}</p>
+        </Fade>
+        <Fade bottom>
+          <div className="flex justify-center mt-4">
+            <p className="text-xl sm:text-2xl md:text-3xl text-pink-700 font-extrabold">
+              {t('home:responsabilidad')}
+            </p>
+            <p className="text-xl sm:text-2xl md:text-3xl text-blue-800 font-extrabold pl-2">
+              {t('home:social')}
+            </p>
+          </div>
+        </Fade>
+
         {/**BLOG */}
-        <div className="p-4 sm:p-8 justify-center">
-          <BlogCarousel posts={posts} />
-        </div>
+        <Fade right>
+          <input
+            className="bg-violeta text-white text-xs rounded-full py-1 px-4 mt-8 ml-8 mb-2"
+            type="button"
+            value={t('home:ver_blog')}
+          />
+          <div className="p-4 sm:p-8 justify-center">
+            <BlogCarousel posts={posts} />
+          </div>
+        </Fade>
       </div>
     </>
   )
